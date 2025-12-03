dc394f62fae1b42fe5fba717a91225b2f0fb9e51
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-12-03 16:31:05 +0900

    [update] version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="defconfig_explainer",
-    version="0.1.0",
+    version="0.2.0",
     description="A tool to analyze and explain Kconfig defconfig files.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
